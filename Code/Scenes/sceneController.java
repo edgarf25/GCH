207d1bec995edb807681c18469437ca208e8329c
edgarf25 <edgarfong25@hotmail.com>
2023-10-18 18:22:24 -0700

    Fixed Scene Controller, it now works properly

--- Code/Scenes/sceneController.java
+++ Code/Scenes/sceneController.java
@@ -3,6 +3,8 @@ package Scenes;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,28 +14,16 @@ public class sceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    int counter = 0;
 
-    String[] str = {"1.FXML", "2.FXML"};
 
-    public void switchToSceneNext(ActionEvent event, int fxml) throws IOException 
+    public void switchToScene2(ActionEvent event) throws IOException 
     {
-        root = FXMLLoader.load(getClass().getResource(str[counter]));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+            root = FXMLLoader.load(getClass().getResource("Dnd Front1.0.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
     }
-
-    public void switchToRace(ActionEvent event) throws IOException 
-    {
-        root = FXMLLoader.load(getClass().getResource("race.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
 
     
 }
