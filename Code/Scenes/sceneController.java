0e780fea60429a97c8295a8891cf200de626dba3
Elijah Munoz <elijahesmunoz@gmail.com>
2023-10-24 11:40:13 -0700

    Added Another package for controls for the drop down menu with no errors runs fine !

--- Code/Scenes/sceneController.java
+++ Code/Scenes/sceneController.java
@@ -1,29 +1,31 @@
 package Scenes;
 
 import java.io.IOException;
-import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.ImageCursor;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.control.ChoiceBox;
 
-public class sceneController  {
+public class sceneController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private ChoiceBox<String> racebox;
+
 
 
     public void switchToScene2(ActionEvent event) throws IOException 
     {
             Image cursor = new Image("/src/sword.png");//changing cursor
-            root = FXMLLoader.load(getClass().getResource("Race-Subrace 1.0.fxml"));
+            root = FXMLLoader.load(getClass().getResource("../application/Race-Subrace 1.0.fxml"));
             stage = (Stage)((Node)event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             scene.setCursor(new ImageCursor(cursor));
