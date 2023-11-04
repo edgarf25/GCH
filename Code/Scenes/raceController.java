7635e5fc1e41c57e20631ae872f0eddab3d5fd3e
Mason Vick <masonvick@outlook.com>
2023-11-03 20:38:03 -0700

    update subraces based on race

--- Code/Scenes/raceController.java
+++ Code/Scenes/raceController.java
@@ -7,8 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-
+import javafx.scene.control.TextArea;
 
 
 public class raceController implements Initializable{
@@ -16,24 +15,30 @@ public class raceController implements Initializable{
     private ChoiceBox<String> myChoiceBox;
     @FXML
     private ChoiceBox<String> subraceChoiceBox;
-    @FXML 
-    private Label raceLabel;
     @FXML
-    private Label classLabel;
-    @FXML
-    private Label abilityLabel;
-    @FXML
-    private Label backgroundLabel;
-    @FXML
-    private Label equipmentLabel;
-    @FXML
-    private Label summaryLabel;
-
+    private TextArea backgroundDescription = new TextArea();
     private String[] racesArray = {"Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"};
     private String[] dwarfSubraces = {"Hill Dwarf", "Mountain Dwarf"};
-    private String[] elfSubraces = {"High Elf", "Wood Elf", "Eladrin Elf"};
-    private String[] halflingSubraces = {"Lightfoot Halfling", "StoutHalfling"};
+    private String[] elfSubraces = {"High Elf", "Wood Elf"};
+    private String[] halflingSubraces = {"Lightfoot Halfling", "Stout Halfling"};
     private String[] gnomeSubraces = {"Rock Gnome", "Deep Gnome"};
+    private String[] descriptions = {"Dwarves are well known for being strong and hardy creatures. Usually, they are portrayed as robust, muscular creatures with a stocky, powerful build. These diligent and hardworking animals are frequently connected to expert craftsmanship and a fondness for mining.",
+                                     "You possess extraordinary resilience, strong intuition, and acute senses as a hill dwarf. In the Dragonlance setting, the debased Klar of Krynn and the banished Neidar are both hill dwarves, as are the gold dwarves of Faerûn in their powerful southern kingdom.",
+                                     "You are tough and resilient as a mountain dwarf, used to living in harsh environments. As a dwarf, you are most likely taller than average and have a tendency toward lighter coloring. Mountain dwarves are the shield dwarves of northern Faerûn, the powerful Hylar clan, and the honorable Daewar clan of Dragonlance.",
+                                     "Elves are renowned for being graceful and airy beings. They are frequently depicted as tall, lean creatures with exquisite features. These magical beings are usually linked to having a strong bond with the natural world and a penchant for magic.",
+                                     "You are a high elf with a sharp mind and a rudimentary understanding of magic. High elves are often depicted as having a sense of superiority and can come across as aloof or reserved when dealing with other races. They take great pride in their heritage and culture.",
+                                     "You have a strong bond with the natural world, forests, and wilderness as a wood elf. They are frequently portrayed as being attuned to their surroundings and possessing traits indicative of their forest-dwelling way of life.",
+                                     "Halflings, often referred to as \"hobbits\" in some fantasy settings, are a small and unassuming race known for their love of simple pleasures, community, and a quiet, peaceful way of life.",
+                                     "Lightfoot halflings are particularly skilled at being stealthy and nimble. They can easily blend into their surroundings and are often known for their innate ability to avoid notice, making them excellent thieves, spies, and rogues.",
+                                     "You're a sturdy halfling with some resistance to poison, making you more resilient than most. Stouts are said to be descended from dwarves. These halflings are known as stronghearts in the Forgotten Realms, and they are primarily found in the southern regions.",
+                                     "Among the common races, humans are the most ambitious and adaptive. Humans are the inventors, the achievers, the world's pioneers, whatever their motivations.",
+                                     "Dragonborn are a humanoid race with dragon-like traits, including scaled skin, breath weapons, and ancestry tied to various dragon types. They often exhibit courage, honor, and a diverse array of abilities based on their dragon lineage.",
+                                     "Gnomes are small, playful humanoid beings known for their inquisitive nature, creativity, and connection to both the natural world and magic. They are skilled artisans and tinkerers, often engaging in playful pranks and lighthearted humor.",
+                                     "Rock gnomes are a subrace of gnomes known for their inventive and clever nature. They are skilled tinkerers, artisans, and craftsmen, often creating gadgets and devices. They possess an affinity for precious stones and a deep connection to the earth",
+                                     "Deep gnomes, also known as \"svirfneblin,\" are a subrace of gnomes adapted to the underground world. They thrive in dark, subterranean environments, possessing stealth, resilience, and magical aptitude. Their close-knit communities, craftsmanship, and quest for knowledge make them intriguing inhabitants of the deep.",
+                                     "Half-elves are a hybrid race with mixed human and elven heritage, resulting in a versatile and charismatic blend of physical features. Half-elves possess enhanced senses and a penchant for exploring the world, making them adaptable and intriguing individuals with a unique place in society.",
+                                     "Half-orcs are a hardy race resulting from the mix of human and orc ancestry, known for their robust physique and physical prowess. Half-orcs excel in combat and are known for their pragmatic, resourceful nature, making them formidable warriors and adventurers.",
+                                     "Tieflings are a distinctive race with infernal ancestry, marked by unique physical traits like horns and tails, as well as supernatural abilities tied to their fiendish lineage. They often face prejudice and struggle with their identity but are known for their resilience and determination."};
 
     private sceneController controller; //created a scenceController instance
 
@@ -53,25 +58,33 @@ public class raceController implements Initializable{
                 // Based on the selected race, add corresponding subraces to the second ChoiceBox
                 if (newValue.equals("Dwarf")) {
                     subraceChoiceBox.getItems().addAll(dwarfSubraces);
+                    backgroundDescription.setText(descriptions[0]);
                 }
                 else if (newValue.equals("Elf"))
                 {
                     subraceChoiceBox.getItems().addAll(elfSubraces);
+                    backgroundDescription.setText(descriptions[3]);
                 }
                 else if (newValue.equals("Halfling"))
                 {
                     subraceChoiceBox.getItems().addAll(halflingSubraces);
+                    backgroundDescription.setText(descriptions[6]);
+                }
+                else if (newValue.equals("Human"))
+                {
+                    backgroundDescription.setText(descriptions[9]);
+                }
+                else if (newValue.equals("Dragonborn"))
+                {
+                    backgroundDescription.setText(descriptions[10]);
                 }
                 else if (newValue.equals("Gnome"))
                 {
                     subraceChoiceBox.getItems().addAll(gnomeSubraces);
+                    backgroundDescription.setText(descriptions[11]);
                 }
             }
         });
-
-        // Add event handlers to the labels
-        
-
     }
 
 
@@ -116,7 +129,7 @@ public class raceController implements Initializable{
         controller = new sceneController();
         controller.switchToScene7(event); // Called the prev method with the empty event
     }
-    
+
 
 
 }
