47406756d37a906282c9d8a09e2edf8e214e4f12
Mason Vick <masonvick@outlook.com>
2023-11-03 18:02:07 -0700

    update subraces based on race

--- Code/Scenes/raceController.java
+++ Code/Scenes/raceController.java
@@ -12,16 +12,50 @@ import javafx.scene.control.ChoiceBox;
 public class raceController implements Initializable{
     @FXML
     private ChoiceBox<String> myChoiceBox;
+    @FXML
+    private ChoiceBox<String> subraceChoiceBox;
     private String[] racesArray = {"Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"};
+    private String[] dwarfSubraces = {"Hill Dwarf", "Mountain Dwarf"};
+    private String[] elfSubraces = {"High Elf", "Wood Elf", "Eladrin Elf"};
+    private String[] halflingSubraces = {"Lightfoot Halfling", "StoutHalfling"};
+    private String[] gnomeSubraces = {"Rock Gnome", "Deep Gnome"};
+
     private sceneController controller; //created a scenceController instance
 
     @Override
-    public void initialize(URL arg0, ResourceBundle arg1) 
-    {
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        // Assuming racesArray and dwarfSubraces are ObservableLists or Lists of String
+
+        // Add items to the first ChoiceBox
         myChoiceBox.getItems().addAll(racesArray);
-        System.out.println(myChoiceBox.getValue());
+
+        // Set up a listener for the first ChoiceBox
+        myChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                // Clear existing items in the second ChoiceBox
+                subraceChoiceBox.getItems().clear();
+
+                // Based on the selected race, add corresponding subraces to the second ChoiceBox
+                if (newValue.equals("Dwarf")) {
+                    subraceChoiceBox.getItems().addAll(dwarfSubraces);
+                }
+                else if (newValue.equals("Elf"))
+                {
+                    subraceChoiceBox.getItems().addAll(elfSubraces);
+                }
+                else if (newValue.equals("Halfling"))
+                {
+                    subraceChoiceBox.getItems().addAll(halflingSubraces);
+                }
+                else if (newValue.equals("Gnome"))
+                {
+                    subraceChoiceBox.getItems().addAll(gnomeSubraces);
+                }
+            }
+        });
     }
-    
+
+
     public void prev(ActionEvent event) throws IOException
     {
         controller = new sceneController();
