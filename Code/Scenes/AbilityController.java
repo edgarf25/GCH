4e18291698f14ad91ae82c7ddf4dfdf01eeaea3a
Mason Vick <masonvick@outlook.com>
2023-11-05 14:16:14 -0800

    added option dropdown for ability choice

--- Code/Scenes/AbilityController.java
+++ Code/Scenes/AbilityController.java
@@ -1,6 +1,77 @@
 package Scenes;
 
-public class AbilityController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.fxml.FXML;
 
+public class AbilityController implements Initializable{
+
+
+    @FXML
+    private ChoiceBox<String> optionsChoiceBox;
+    private String[] options = {"Standard Array", "Point Buy", "Randomize"};
+   private sceneController controller; //created a scenceController instance
+
+
+    public void prev(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.prev(event); // Called the prev method with the empty event
+    }
+
+    public void next(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.next(event); // Called the prev method with the empty event
+    }
+
+    public void initialize(URL arg0, ResourceBundle arg1)
+    {
+        optionsChoiceBox.getItems().addAll(options);
+        optionsChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->
+        {
+
+        });
+    };
+
+    public void switchToScene3(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene3(event); // Called the prev method with the empty event
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene4(event); // Called the prev method with the empty event
+    }
+    public void switchToScene4(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene4(event); // Called the prev method with the empty event
+    }
+
+    public void switchToScene5(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene5(event); // Called the prev method with the empty event
+    }
+
+    public void switchToScene6(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene6(event); // Called the prev method with the empty event
+    }
+
+    public void switchToScene7(ActionEvent event) throws IOException
+    {
+        controller = new sceneController();
+        controller.switchToScene7(event); // Called the prev method with the empty event
+    }
 
 }
