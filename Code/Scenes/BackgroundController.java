55c25d751b73f029ba1984cf6a51c75d92f23583
aaronhamilton <aham9695@gmail.com>
2023-11-06 06:13:33 -0800

    summary started on

--- Code/Scenes/BackgroundController.java
+++ Code/Scenes/BackgroundController.java
@@ -40,6 +40,8 @@ public class BackgroundController implements Initializable {
                                              "War has been your life for as long as you care to remember. You trained as a youth, studied the use of weapons and armor, learned basic survival techniques, including how to stay alive on the battlefield. You might have been part of a standing national army or a mercenary company, or perhaps a member of a local militia who rose to prominence during a recent war.\n" +
                                                      "\n" +
                                                      "When you choose this background, work with your DM to determine which military organization you were a part of, how far through its ranks you progressed, and what kind of experiences you had during your military career. Was it a standing army, a town guard, or a village militia? Or it might have been a noble’s or merchant’s private army, or a mercenary company."};
+
+    private sceneController controller; //created a scenceController instance
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         backgroundChoice.getItems().addAll(backgrounds);
@@ -70,8 +72,6 @@ public class BackgroundController implements Initializable {
             backgroundTextBox.setText(descriptions[6]);
         }
     }
-
-    private sceneController controller; //created a scenceController instance
 
     public void prev(ActionEvent event) throws IOException
     {
