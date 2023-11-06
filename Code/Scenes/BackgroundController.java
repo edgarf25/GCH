60f4692103041110a6a037d96219a01983658301
aaronhamilton <aham9695@gmail.com>
2023-11-06 06:30:57 -0800

    summary started on

--- Code/Scenes/BackgroundController.java
+++ Code/Scenes/BackgroundController.java
@@ -2,9 +2,7 @@ package Scenes;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Objects;
 import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
