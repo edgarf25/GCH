81a5c4460a4306f5cc2f97e957b20f6f0747ea49
aaronhamilton <aham9695@gmail.com>
2023-11-06 09:54:08 -0800

    summary started on

--- Code/Scenes/BackgroundController.java
+++ Code/Scenes/BackgroundController.java
@@ -48,7 +48,7 @@ public class BackgroundController implements Initializable {
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
         backgroundTextBox.setWrapText(true);
-        backgroundTextBox.setFont(Font.font(36));
+        backgroundTextBox.setFont(Font.font(24));
         backgroundChoiceBox.getItems().addAll(backgrounds);
         backgroundChoiceBox.setOnAction(this::getBackground);
     }
