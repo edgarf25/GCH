b1d3556e83b2b9fca416e1f9909cb77a8ab9f31f
Elijah Munoz <elijahesmunoz@gmail.com>
2023-11-06 17:59:02 -0800

    Equipment - Updated Layout - Added to List Display - Add all Button works - Font change! BETTER

--- Code/Scenes/EquipmentController.java
+++ Code/Scenes/EquipmentController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 
 public class EquipmentController implements Initializable {
      @FXML
@@ -15,8 +16,9 @@ public class EquipmentController implements Initializable {
      private ChoiceBox<String> myChoiceBox1;   //ARMOR-DROP-DOWN
      @FXML
      private ChoiceBox<String>  myChoiceBox11;  //Possessions-DROP-DOWN
-//    @FXML
-//     private TextArea listItems = new TextArea();  // DISPLAYING ITEMS ON LIST
+    @FXML
+    private TextArea TextAreaList;  // DISPLAYING ITEMS ON LIST
+    private static String ListText = "";
     private final String[] weaponArray = {"Club","Dagger", "GreatClub", "HandAxe", "Javelin","Light Hammer", "Mace", "Quarterstaff", "Sickle", "Spear"};
     // WEAPON ARRAY CONTAINING LIST OF ARRAY OF WEAPONS MELEE
     private final String[] armorArray ={"Light Armor","Medium Armor","Heavy Armor","Shield"};
@@ -30,10 +32,39 @@ public class EquipmentController implements Initializable {
         myChoiceBox0.getItems().addAll(weaponArray);  // GET ALL WEAPONS IN ARRAY
         myChoiceBox1.getItems().addAll(armorArray);  //  GET ALL ARMOR   IN ARRAY
         myChoiceBox11.getItems().addAll(posArray);  //   GET ALL POS     IN ARRAY
+        myChoiceBox0.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addToString("Weapon: " + newValue + "\n");
 
+            }
+        });
+        myChoiceBox1.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addToString("Armor: " + newValue + "\n");
+
+            }
+        });
+        myChoiceBox11.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                addToString("Possessions: " + newValue + "\n");
+
+            }
+        });
 
     }
+    public void addToString(String s)
+    {
+        if (s != null)
+        {
+            ListText += s;
 
+        }
+    }
+     public  void addAll() {
+        if(ListText != null ){
+            TextAreaList.setText(ListText);
+        }
+     }
     private sceneController controller; //created a scenceController instance
 
     public void prev(ActionEvent event) throws IOException
