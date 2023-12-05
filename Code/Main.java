9d1515ffafc8edbfba5125ec1d1e8cdb172d65c1
edgarf25 <edgarfong25@hotmail.com>
2023-12-04 23:16:33 -0800

    Added data getset for throws

--- Code/Main.java
+++ Code/Main.java
@@ -11,21 +11,23 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) 
-    {
-        try
-        {
-            Image cursor = new Image("/src/sword.png"); //loading custom cursor
+    public void start(Stage primaryStage) {
+        try {
+            Image cursor = new Image("/src/sword.png"); // loading custom cursor
             Parent root = FXMLLoader.load(getClass().getResource("/Scenes/Welcome.fxml"));
+            
+            // Load the CSS file
             Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
             primaryStage.setScene(scene);
             scene.setCursor(new ImageCursor(cursor));
             primaryStage.setResizable(false);
             primaryStage.show();
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }
     }
+}
 
     public static void main(String[] args) {
         launch(args);
