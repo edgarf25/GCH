59cca4fd40ba0e06400cefca814adab81d36479a
Mason Vick <masonvick@outlook.com>
2023-12-05 02:46:28 -0800

    equipment and spells save over normally everything is perfect yay

--- Code/Scenes/EquipmentController.java
+++ Code/Scenes/EquipmentController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
@@ -12,14 +13,14 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class EquipmentController implements Initializable {
-     @FXML
-     private ChoiceBox<String> weaponChoiceBox;        //WEAPON-DROP-DOWN
-     @FXML
-     private ChoiceBox<String> armorChoiceBox;        //ARMOR-DROP-DOWN
-     @FXML
-     private ChoiceBox<String> possessionChoiceBox;   //Possessions-DROP-DOWN
     @FXML
-    private ChoiceBox<String>  spellChoiceBox;        ///SPELL-DROP-DOWN
+    private ChoiceBox<String> weaponChoiceBox;        //WEAPON-DROP-DOWN
+    @FXML
+    private ChoiceBox<String> armorChoiceBox;        //ARMOR-DROP-DOWN
+    @FXML
+    private ChoiceBox<String> possessionChoiceBox;   //Possessions-DROP-DOWN
+    @FXML
+    private ChoiceBox<String> spellChoiceBox;        ///SPELL-DROP-DOWN
     @FXML
     private TextField GoldInput;  //GOLD  INPUT TEXT BOX
     @FXML
@@ -29,29 +30,29 @@ public class EquipmentController implements Initializable {
     private String armorText = "";
     private String posText = "";
     private String spellText = "";
-    private final String[] weaponArray = {"Club","Dagger", "GreatClub", "HandAxe", "Javelin","Light Hammer", "Mace", "Quarterstaff", "Sickle", "Spear"};
+    private final String[] weaponArray = {"Club", "Dagger", "GreatClub", "HandAxe", "Javelin", "Light Hammer", "Mace", "Quarterstaff", "Sickle", "Spear"};
     // WEAPON ARRAY CONTAINING LIST OF ARRAY OF WEAPONS MELEE
-    private final String[] armorArray ={"Light Armor","Medium Armor","Heavy Armor"};
+    private final String[] armorArray = {"Light Armor", "Medium Armor", "Heavy Armor"};
     // ARMOR CONTAINING LIST OF ARRAY ARMOR
-    private final String[] posArray = {"DINGLE","SHEMCKLES","Shield"};
-    private final String[] BarbArray  = {"Vicious Mockery", "Thunderwave", "Healing Word"};
-    private final String[] clericArray  = {"Sacred Flame", "Cure Wounds ",  "Spirit Guardians" };
+    private final String[] posArray = {"DINGLE", "SHEMCKLES", "Shield"};
+    private final String[] BarbArray = {"Vicious Mockery", "Thunderwave", "Healing Word"};
+    private final String[] clericArray = {"Sacred Flame", "Cure Wounds ", "Spirit Guardians"};
 
-    private final String[] druidArray = {"Druid-craft","Entangle", "Moonbeam "};
-    private final String[] sorcArray = {"Druid-craft","Entangle", "Moonbeam "};
+    private final String[] druidArray = {"Druid-craft", "Entangle", "Moonbeam "};
+    private final String[] sorcArray = {"Druid-craft", "Entangle", "Moonbeam "};
     private final String[] wizardArray = {"Mage Hand", "Detect Magic", "Fireball"};
     private final String[] warlockArray = {"Eldritch Blast", "Hex", "Doom"};
     private final String[] paladinArray = {"Divine Smite", "Lay on Hands"};
     private final String[] rangerArray = {"Hunter's Mark", "Cordon of Arrows"};
 
-    private final String[] rogueArray = {"Arcane Trickster", "Mage Hand", " Mage Armor" };
+    private final String[] rogueArray = {"Arcane Trickster", "Mage Hand", " Mage Armor"};
     private final String[] monkArray = {"Fist of Unbroken Air", "Flowing River"};
 
     private int amountOfGold = 0;
     private String goldLine = "";
     CharacterData characterData = CharacterData.getInstance();
 
-    public void initialize(URL arg0, ResourceBundle arg1){
+    public void initialize(URL arg0, ResourceBundle arg1) {
         //  Weapons Array Lists of String
         // Add items to the first ChoiceBox
         specifySpells();
@@ -87,154 +88,172 @@ public class EquipmentController implements Initializable {
 
     }
 
-    public void specifySpells()
-    {
-        if (ClassController.userClass != null)
-        {
+    public void specifySpells() {
+        if (ClassController.userClass != null) {
 
-            if(ClassController.userClass.equals("Bard"))
-            {
+            if (ClassController.userClass.equals("Bard")) {
                 spellChoiceBox.getItems().addAll(BarbArray);
-            }
-            else if(ClassController.userClass.equals("Cleric"))
-            {
+            } else if (ClassController.userClass.equals("Cleric")) {
                 spellChoiceBox.getItems().addAll(clericArray);
-            }
-            else if(ClassController.userClass.equals("Druid"))
-            {
+            } else if (ClassController.userClass.equals("Druid")) {
                 spellChoiceBox.getItems().addAll(druidArray);
-            }
-            else if(ClassController.userClass.equals("Monk"))
-            {
-                 spellChoiceBox.getItems().addAll(monkArray);
-            }
-            else if(ClassController.userClass.equals("Paladin"))
-            {
+            } else if (ClassController.userClass.equals("Monk")) {
+                spellChoiceBox.getItems().addAll(monkArray);
+            } else if (ClassController.userClass.equals("Paladin")) {
                 spellChoiceBox.getItems().addAll(paladinArray);
-            }
-            else if(ClassController.userClass.equals("Ranger"))
-            {
+            } else if (ClassController.userClass.equals("Ranger")) {
                 spellChoiceBox.getItems().addAll(rangerArray);
-            }
-            else if(ClassController.userClass.equals("Rogue"))
-            {
-                 spellChoiceBox.getItems().addAll(rogueArray);
-            }
-            else if(ClassController.userClass.equals("Sorcerer"))
-            {
+            } else if (ClassController.userClass.equals("Rogue")) {
+                spellChoiceBox.getItems().addAll(rogueArray);
+            } else if (ClassController.userClass.equals("Sorcerer")) {
                 spellChoiceBox.getItems().addAll(sorcArray);
-            }
-            else if(ClassController.userClass.equals("Warlock"))
-            {
+            } else if (ClassController.userClass.equals("Warlock")) {
                 spellChoiceBox.getItems().addAll(warlockArray);
-            }
-            else if(ClassController.userClass.equals("Wizard"))
-            {
+            } else if (ClassController.userClass.equals("Wizard")) {
                 spellChoiceBox.getItems().addAll(wizardArray);
             }
         }
     }
+
     /// Inistialize  ArrayList
-    public void addToString(String s, int e)
-    {
-        if (e == 0)
-        {
+    public void addToString(String s, int e) {
+        if (e == 0) {
             weaponText = s; // WEAPON
-        }
-        else if (e == 1)
-        {
+        } else if (e == 1) {
             armorText = s; // ARMOR
-        }
-        else if (e == 2)
-        {
+        } else if (e == 2) {
             posText = s;  // POS
-        }
-        else if (e == 3)
-        {
+        } else if (e == 3) {
             spellText = s; // SPELL
         }
 
     }
-    public void addWeapon()
-    {
-        if (weaponText != null)
-        {
-            ListText += weaponText;
-            TextAreaList.setText(ListText);
-            characterData.setEquipment(ListText);
+
+    public void addWeapon() {
+        if (weaponText != null) {
+            if (characterData.getEquipment() != null) {
+                characterData.setEquipment(characterData.getEquipment() + weaponText);
+                if (characterData.getSpells() == null) {
+                    TextAreaList.setText(characterData.getEquipment());
+                } else {
+                    TextAreaList.setText(characterData.getEquipment() + characterData.getSpells());
+                }
+            } else if (characterData.getSpells() != null) {
+                TextAreaList.setText(weaponText + characterData.getSpells());
+                characterData.setEquipment(weaponText);
+            } else {
+                characterData.setEquipment(weaponText);
+                TextAreaList.setText(weaponText);
+            }
             weaponText = "";
-
         }
     }
-    public void addArmor()
-    {
-        if (armorText != null)
-        {
-            ListText += armorText;
-            TextAreaList.setText(ListText);
-            characterData.setEquipment(ListText);
+
+    public void addArmor() {
+        if (armorText != null) {
+            if (characterData.getEquipment() != null) {
+                characterData.setEquipment(characterData.getEquipment() + armorText);
+                if (characterData.getSpells() == null) {
+                    TextAreaList.setText(characterData.getEquipment());
+                } else {
+                    TextAreaList.setText(characterData.getEquipment() + characterData.getSpells());
+                }
+            } else if (characterData.getSpells() != null) {
+                characterData.setEquipment(armorText);
+                TextAreaList.setText(armorText + characterData.getSpells());
+            } else {
+                characterData.setEquipment(armorText);
+                TextAreaList.setText(armorText);
+            }
             armorText = "";
-
         }
     }
-    public void addPossession()
-    {
-        if (posText != null)
-        {
-            ListText += posText;
-            TextAreaList.setText(ListText);
-            characterData.setEquipment(ListText);
+
+    public void addPossession() {
+        if (posText != null) {
+            if (characterData.getEquipment() != null) {
+                characterData.setEquipment(characterData.getEquipment() + posText);
+                if (characterData.getSpells() == null) {
+                    TextAreaList.setText(characterData.getEquipment());
+                } else {
+                    TextAreaList.setText(characterData.getEquipment() + characterData.getSpells());
+                }
+            } else if (characterData.getSpells() != null) {
+                characterData.setEquipment(posText);
+                TextAreaList.setText(posText + characterData.getSpells());
+            } else {
+                characterData.setEquipment(posText);
+                TextAreaList.setText(posText);
+            }
             posText = "";
         }
     }
-    public void addSpell()
-    {
-        if (spellText != null)
-        {
-            ListText += spellText;
-            TextAreaList.setText(ListText);
-            if (characterData.getSpells() != null)
-            {
+
+    public void addSpell() {
+        if (spellText != null) {
+            if (characterData.getSpells() != null) {
                 characterData.setSpells(characterData.getSpells() + spellText);
-            }
-            else
-            {
+                if (characterData.getEquipment() == null) {
+                    TextAreaList.setText(characterData.getSpells());
+                } else {
+                    TextAreaList.setText(characterData.getEquipment() + characterData.getSpells());
+                }
+            } else if (characterData.getEquipment() != null) {
                 characterData.setSpells(spellText);
+                TextAreaList.setText(characterData.getEquipment() + spellText);
+            } else {
+                characterData.setSpells(spellText);
+                TextAreaList.setText(spellText);
             }
             spellText = "";
         }
     }
-     public void addGold() {
-        // System.out.println(amountOfGold);
-         if (GoldInput != null && !GoldInput.getText().isEmpty()) {
-             StringBuffer text = new StringBuffer(ListText);
-             amountOfGold += Integer.valueOf(GoldInput.getText());
-             String sub = "";
-             if (ListText.contains("Gold")) {
-                 goldLine = "Gold: "  +  amountOfGold + "g";
-                 System.out.println(goldLine);
-                 text.replace(ListText.indexOf("Gold") , ListText.indexOf("g")+1, goldLine);
-                 sub = ListText.substring(ListText.indexOf("Gold") , ListText.indexOf("g")+1);
-                 ListText = ListText.replace(sub, goldLine);
-             } else {
-                 ListText +=  "Gold: "  +  amountOfGold + "g\n";
-             }
-             TextAreaList.setText(ListText);
-             characterData.setEquipment(ListText);
-             GoldInput.setText("");
-         }
 
-     }
-    public void checker() //checking that age only has number inputs
-    {
-        try
-        {
-            Integer.parseInt(GoldInput.getText());
-        } catch (NumberFormatException nfe){
+    public void addGold() {
+        // System.out.println(amountOfGold);
+        String list = characterData.getEquipment();
+        if (GoldInput != null && !GoldInput.getText().isEmpty()) {
+            if (list != null) {
+                StringBuffer text = new StringBuffer(list);
+                amountOfGold += Integer.valueOf(GoldInput.getText());
+                String sub = "";
+                if (list.contains("Gold")) {
+                    goldLine = "Gold: " + amountOfGold + "g";
+                    System.out.println(goldLine);
+                    text.replace(list.indexOf("Gold"), list.indexOf("g") + 1, goldLine);
+                    sub = list.substring(list.indexOf("Gold"), list.indexOf("g") + 1);
+                    characterData.setEquipment(list.replace(sub, goldLine));
+                } else {
+                    characterData.setEquipment(list + "Gold: " + amountOfGold + "g\n");
+                }
+                if (characterData.getSpells() == null) {
+                    TextAreaList.setText(characterData.getEquipment());
+                } else {
+                    TextAreaList.setText(characterData.getEquipment() + characterData.getSpells());
+                }
+            } else if (characterData.getSpells() != null) {
+                characterData.setEquipment("Gold: " + GoldInput.getText() + "g\n");
+                TextAreaList.setText("Gold: " + GoldInput.getText() + "g\n" + characterData.getSpells());
+                addGold();
+            } else {
+                characterData.setEquipment("Gold: " + GoldInput.getText() + "g\n");
+                TextAreaList.setText("Gold: " + GoldInput.getText() + "g\n");
+                addGold();
+            }
             GoldInput.setText("");
         }
     }
-     public void clear(){
+
+    public void checker() //checking that age only has number inputs
+    {
+        try {
+            Integer.parseInt(GoldInput.getText());
+        } catch (NumberFormatException nfe) {
+            GoldInput.setText("");
+        }
+    }
+
+    public void clear() {
         ListText = "";
         TextAreaList.setText("");
         weaponText = "";
@@ -246,58 +265,47 @@ public class EquipmentController implements Initializable {
         armorChoiceBox.getSelectionModel().clearSelection();
         possessionChoiceBox.getSelectionModel().clearSelection();
         spellChoiceBox.getSelectionModel().clearSelection();
-     }
-     
+    }
+
     private sceneController controller; //created a scenceControl7ler instance
 
 
-
-
-
-    public void prev(ActionEvent event) throws IOException
-    {
+    public void prev(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.prev(event); // Called the prev method with the empty event
     }
 
-    public void next(ActionEvent event) throws IOException
-    {
+    public void next(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.next(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene2(ActionEvent event) throws IOException
-    {
+    public void switchToScene2(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene2(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene3(ActionEvent event) throws IOException
-    {
+    public void switchToScene3(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene3(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene4(ActionEvent event) throws IOException
-    {
+    public void switchToScene4(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene4(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene5(ActionEvent event) throws IOException
-    {
+    public void switchToScene5(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene5(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene6(ActionEvent event) throws IOException
-    {
+    public void switchToScene6(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene6(event); // Called the prev method with the empty event
     }
 
-    public void switchToScene7(ActionEvent event) throws IOException
-    {
+    public void switchToScene7(ActionEvent event) throws IOException {
         controller = new sceneController();
         controller.switchToScene7(event); // Called the prev method with the empty event
     }
