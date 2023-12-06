8e908a094adc9b15d20e53a40492bb173bb85d0d
Edgar Fong <edgarfong25@hotmail.com>
2023-12-05 17:43:56 -0800

    Can acess settings

--- Code/Scenes/Settings.java
+++ Code/Scenes/Settings.java
@@ -1,10 +1,21 @@
 package Scenes;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
 public class Settings {
+
+    sceneController sceneController = new sceneController();
 
     public void candyCaneCursor()
     {
         
+    }
+
+    public void returnScene(ActionEvent event) throws IOException
+    {
+        sceneController.switchToScene1(event); // Called the prev method with the empty event
     }
 
     
--- Code/Scenes/sceneController.java
+++ Code/Scenes/sceneController.java
@@ -17,6 +17,7 @@ public class sceneController {
 
 
     String array[] = {"Welcome.fxml","Race-Subrace 1.0.fxml","Class1.0.fxml","Ability1.0.fxml","Background1.0.fxml","Equipment1.0.fxml","Summary1.0.fxml"};
+    String otherScenes[] = {"Settings.fxml"};
     public static int counter = 0;
 
 
@@ -44,18 +45,22 @@ public class sceneController {
         stage.show(); 
     }
 
-     public void loadSceneSummary(ActionEvent event) throws IOException
+     public void loadOtherScenes(ActionEvent event) throws IOException
     {
         Image cursor = new Image("/src/sword.png");//changing cursor
-        root = FXMLLoader.load(getClass().getResource(array[counter]));
+        root = FXMLLoader.load(getClass().getResource(otherScenes[counter]));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
-        String css = this.getClass().getResource("summary.css").toExternalForm();
-        scene.getStylesheets().add(css);
         scene.setCursor(new ImageCursor(cursor));
         stage.setScene(scene);
         stage.centerOnScreen(); // Centering the stage on the screen
         stage.show(); 
+    }
+
+     public void switchToScene1(ActionEvent event) throws IOException 
+    {
+        counter = 0;
+        loadScene(event);
     }
 
     public void switchToScene2(ActionEvent event) throws IOException 
@@ -92,6 +97,12 @@ public class sceneController {
     {
         counter = 6;
         loadScene(event);
+    }
+    
+    public void switchToSettings(ActionEvent event) throws IOException
+    {
+        counter = 0;
+        loadOtherScenes(event);
     }
 
    
